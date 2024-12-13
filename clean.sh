d85db408b607b60f7145e106d859f4344dc0544b
Aadish C <aadishchaubal@hotmail.com>
2024-12-12 22:22:32 -0500

    move nsight and cuda cleanup into separate function

--- clean.sh
+++ clean.sh
@@ -33,16 +33,81 @@ clear_cache(){
 	calculate_GB $cache
 	if [ "$fin" != "0.00 GB" ];then
 	        calculate_total
-		if [ "$1" == "DNF cache" ];then
-		    dnf clean all > /dev/null
-		elif [ "$1" == "Edge cache" ];then
-		    rm -r $2/* "$3"/*
-		else
-		    rm -rf $2/*
-		fi
+	        case $1 in
+			"DNF cache")
+			    dnf clean all > /dev/null
+			;;
+			"Edge cache")
+			    rm -r $2/* "$3"/*
+			;;
+			*)
+			    rm -rf $2/*
+			;;
+		esac
 		echo -e "Deleted \033[0;35m$1: \033[0m"$fin
                 add_to_log "$1" "$fin"
     fi
+}
+
+clear_nvidia(){
+	nvidia=(nsight-compute nsight-systems)
+	for i in "${nvidia[@]}"
+	do
+		x=$(ls -l "$3/$i" | grep -c ^d)
+		if [ $x -gt 1 ];then
+			readarray a < <(ls "$3/$i")
+			echo -e "Total folders in \033[0;35m$i\033[0m: \033[0;36m${#a[@]}\033[0m"
+			IFS=$'\n'           ## only word-split on '\n'
+			a=( $(printf "%s\n" ${a[@]} | sort -r ) )  ## reverse sort
+			echo -e "Keeping \033[0;32m$a\033[0m"
+			unset a[0]
+			cache=0
+			j="0"
+			for j in "${a[@]}"
+			do
+			   nvidia_cache=$(du -s $3/$i/$j | awk '{print $1}')
+			   cache=$(($cache + $nvidia_cache))
+			   sudo rm -rf "$3/$i/$j"
+			   echo -e "Deleted \033[0;31m$j: \033[0m"$(convert_to_GB $nvidia_cache $i)
+			done
+			add_to_log "$1-$i" $(convert_to_GB $cache)
+			calculate_total
+		fi
+	done
+
+	x=$(ls "$4" | grep "$2-" | wc -l)
+	if [ $x -gt 2 ];then
+		readarray a < <(ls $4 | grep "$2-[1-9][1-9]$" | sort -r)
+		readarray b < <(ls $4 | grep "$2-[1-9][1-9].[1-9]$" | sort -r)
+		echo -e "Total folders of major versions \033[0;35m$2-xx\033[0m: \033[0;36m${#a[@]}\033[0m"
+		echo -e "Total folders of minor versions \033[0;35m$2-xx.x\033[0m: \033[0;36m${#b[@]}\033[0m"
+		IFS=$'\n'           ## only word-split on '\n'
+		a=( $(printf "%s\n" ${a[@]}) )
+		b=( $(printf "%s\n" ${b[@]}) )
+		if [ ${#a[@]} -gt 1 ];then
+			echo -e "Keeping \033[0;32m$a\033[0m"
+			unset a[0]
+			for i in "${a[@]}"
+			do
+			sudo unlink "$4/$i"
+			echo -e "Deleted \033[0;31m$i\033[0m"
+			done
+		fi
+		if [ ${#b[@]} -gt 1 ];then
+			echo -e "Keeping \033[0;32m$b\033[0m"
+			unset b[0]
+			cache=0
+			for i in "${b[@]}"
+			do
+			cuda_cache=$(du -s $4/$i | awk '{print $1}')
+			cache=$(($cache + $cuda_cache))
+			sudo rm -rf "$4/$i"
+			echo -e "Deleted \033[0;31m$i: \033[0m"$(convert_to_GB $cuda_cache)
+			done
+			add_to_log "$1-$2" $(convert_to_GB $cache)
+			calculate_total
+		fi
+	fi
 }
 
 clear_log
@@ -55,65 +120,7 @@ clear_cache "Firefox cache" "${path[0]}/.cache/mozilla/firefox/$(ls ${path[0]}/.
 clear_cache "DNF cache" "${path[1]}/cache/libdnf5"
 clear_cache "Coredumps" "${path[1]}/lib/systemd/coredump"
 clear_cache "Journal logs" "${path[1]}/log/journal"
-
-nvidia=(nsight-compute nsight-systems)
-for i in "${nvidia[@]}"
-do
-	x=$(ls -l "/opt/nvidia/$i" | grep -c ^d)
-	if [ $x -gt 1 ];then
-		readarray a < <(ls "/opt/nvidia/$i")
-		echo -e "Total folders in \033[0;35m$i\033[0m: \033[0;36m${#a[@]}\033[0m"
-		IFS=$'\n'           ## only word-split on '\n'
-		a=( $(printf "%s\n" ${a[@]} | sort -r ) )  ## reverse sort
-		echo -e "Keeping \033[0;32m$a\033[0m"
-		unset a[0]
-	        cache=0
-	        j="0"
-		for j in "${a[@]}"
-		do
-		   nvidia_cache=$(du -s /opt/nvidia/$i/$j | awk '{print $1}')
-		   cache=$(($cache + $nvidia_cache))
-		   sudo rm -rf "/opt/nvidia/$i/$j"
-		   echo -e "Deleted \033[0;31m$j: \033[0m"$(convert_to_GB $nvidia_cache $i)
-		done
-		add_to_log $i $(convert_to_GB $cache)
-		calculate_total
-	fi
-done
-
-x=$(ls "/usr/local" | grep "cuda-" | wc -l)
-if [ $x -gt 2 ];then
-	readarray a < <(ls /usr/local | grep "cuda-[1-9][1-9]$" | sort -r)
-	readarray b < <(ls /usr/local | grep "cuda-[1-9][1-9].[1-9]$" | sort -r)
-	echo -e "Total folders of major versions \033[0;35mcuda-xx\033[0m: \033[0;36m${#a[@]}\033[0m"
-	echo -e "Total folders of minor versions \033[0;35mcuda-xx.x\033[0m: \033[0;36m${#b[@]}\033[0m"
-	IFS=$'\n'           ## only word-split on '\n'
-	a=( $(printf "%s\n" ${a[@]}) )
-	b=( $(printf "%s\n" ${b[@]}) )
-	if [ ${#a[@]} -gt 1 ];then
-		echo -e "Keeping \033[0;32m$a\033[0m"
-		unset a[0]
-		for i in "${a[@]}"
-		do
-		   sudo unlink "/usr/local/$i"
-		   echo -e "Deleted \033[0;31m$i\033[0m"
-		done
-	fi
-	if [ ${#b[@]} -gt 1 ];then
-		echo -e "Keeping \033[0;32m$b\033[0m"
-		unset b[0]
-		cache=0
-		for i in "${b[@]}"
-		do
-		   cuda_cache=$(du -s /usr/local/$i | awk '{print $1}')
-		   cache=$(($cache + $cuda_cache))
-		   sudo rm -rf "/usr/local/$i"
-		   echo -e "Deleted \033[0;31m$i: \033[0m"$(convert_to_GB $cuda_cache)
-		done
-		add_to_log "Cuda" $(convert_to_GB $cache)
-		calculate_total
-	fi
-fi
+clear_nvidia "Nvidia" "cuda" "/opt/nvidia" "/usr/local"
 
 total=$(convert_to_GB $total)
 add_to_log "Total storage recovered" "$total"
